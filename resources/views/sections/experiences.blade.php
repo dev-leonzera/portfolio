a8b90d2597d73906bb67ed987bca4b1f3f067604
leonardo.hora <leonardo.hora@picsel.com.br>
2023-07-25 00:42:00 -0300

    Alterações

---
+++ resources/views/sections/experiences.blade.php
@@ -0,0 +1,133 @@
+<div class="portfolio-experience">
+    <div class="portfolio-experience-item">
+        <h2>AR Consultoria em Informática</h2>
+        <h3>Desenvolvedor Fullstack</h3>
+        <br>
+        <h4>Dez 2020 - Set 2021 </h4>
+        <br>
+        <p>
+            Responsável pela manutenção de projetos atuais e desenvolvimento de novas funcionalidades para
+            sistemas web
+            internos e externos, e websites, utilizando as tecnologias:
+        </p>
+        <br>
+        <ul id="tech-list">
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/css3.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/javascript--v1.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/bootstrap.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/typescript.png" />
+            </li>
+
+            <li>
+                <img src="https://img.icons8.com/officel/50/000000/php-logo.png" />
+            </li>
+
+            <li>
+                <img src="https://img.icons8.com/color/50/angularjs.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/mysql-logo.png" />
+            </li>
+        </ul>
+
+        <br>
+    </div>
+    <hr>
+    <div class="portfolio-experience-item">
+        <h2>Nova M3</h2>
+        <h3>Desenvolvedor Fullstack</h3>
+        <br>
+        <h4>Jan 2022 - Fev 2023</h4>
+        <br>
+        <p>
+            Atuando como desenvolvedor web fullstack na manutenção de projetos atuais e desenvolvimento de
+            novos
+            projetos e funcionalidades, utilizando as tecnologias:
+        </p>
+        <br>
+        <ul id="tech-list">
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/css3.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/javascript--v1.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/bootstrap.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/fluency/50/000000/laravel.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/officel/50/000000/php-logo.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/docker.png" alt="">
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/mysql-logo.png" />
+            </li>
+            <li>
+                <img
+                    src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/50/000000/external-vuejs-an-open-source-javascript-framework-for-building-user-interfaces-and-single-page-applications-logo-shadow-tal-revivo.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/react-native.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/amazon-web-services.png" />
+            </li>
+        </ul>
+        <br>
+    </div>
+    <hr>
+    <div class="portfolio-experience-item">
+        <h2>Picsel</h2>
+        <h3>Desenvolvedor Fullstack</h3>
+        <br>
+        <h4>Mar 2023 - Jul 2023</h4>
+        <br>
+        <p>
+            Atuando como desenvolvedor web fullstack na manutenção e implementação de novas funcionalidades
+            na plataforma web da empresa,
+            utilizando as tecnologias:
+        </p>
+        <br>
+        <ul id="tech-list">
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/css3.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/typescript.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/officel/50/000000/php-logo.png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/000000/angularjs.png" alt="">
+            </li>
+            <li>
+                <img src="https://img.icons8.com/?id=38561&format=png" />
+            </li>
+            <li>
+                <img src="https://img.icons8.com/color/50/amazon-web-services.png" />
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ resources/views/sections/projects.blade.php
@@ -0,0 +1,14 @@
+<ul>
+    <li>
+        <img src="img/secrypter.jpg" alt="2" />
+        <div>
+            <h2>Secrypter</h2>
+            {{-- <a href="#">Saiba mais</a> --}}
+            <br />
+            <br />
+            <p>
+                Gerador de senhas aleatórias
+            </p>
+        </div>
+    </li>
+</ul>
